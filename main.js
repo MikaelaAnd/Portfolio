e613fae614e6f72357df2f6ad6b6d830245d41fe
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-04 21:33:57 +0100

    fixed the slideshow temporary

---
+++ main.js
@@ -0,0 +1,25 @@
+window.onload = main;
+
+function main() {
+    startSlideShowAnimation();
+}
+
+function startSlideShowAnimation() {
+    setInterval(changeActiveImage, 2000);
+        changeActiveImage();
+}
+
+function changeActiveImage() {
+    const images = document.querySelectorAll('.slideshow-container > img');
+
+        for(let i = 0; i < images.length; i++) {
+            if (images[i].classList.contains('show')) {
+                images[i].classList.remove('show');
+
+                let nextIndex = (i + 1) % images.length;
+
+                images[nextIndex].classList.add('show');
+                break;
+            }
+        }
+}
