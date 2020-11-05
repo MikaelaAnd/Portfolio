9904099ac37ea628f4fd86a2c3544ed2f89ba85f
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-05 15:44:25 +0100

    added some not-working-js at the moment

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
+// window.onload = main;
+
+// function main() {
+//     startSlideShowAnimation();
+// }
+
+// function startSlideShowAnimation() {
+//     setInterval(changeActiveImage, 2000);
+//         changeActiveImage();
+// }
+
+// function changeActiveImage() {
+//     const images = document.querySelectorAll('.slideshow-container > img');
+
+//         for(let i = 0; i < images.length; i++) {
+//             if (images[i].classList.contains('show')) {
+//                 images[i].classList.remove('show');
+
+//                 let nextIndex = (i + 1) % images.length;
+
+//                 images[nextIndex].classList.add('show');
+//                 break;
+//             }
+//         }
+// }// SLIDESHOW
+
 window.onload = main;
 
 function main() {
-    startSlideShowAnimation();
+    clickPictures();
 }
 
-function startSlideShowAnimation() {
-    setInterval(changeActiveImage, 2000);
-        changeActiveImage();
+function clickPictures() {
+    const musicPicture = document.getElementById('musicPicture');
+    musicPicture.onclick = showMusicPage();
 }
 
-function changeActiveImage() {
-    const images = document.querySelectorAll('.slideshow-container > img');
-
-        for(let i = 0; i < images.length; i++) {
-            if (images[i].classList.contains('show')) {
-                images[i].classList.remove('show');
-
-                let nextIndex = (i + 1) % images.length;
-
-                images[nextIndex].classList.add('show');
-                break;
-            }
-        }
-}
+function showMusicPage() 
