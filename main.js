e9ed1fc40c875ebfdaa8570e2b0fd9f1d9f630fc
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-06 11:53:56 +0100

    added some JS

--- main.js
+++ main.js
@@ -26,3 +26,29 @@
 
 window.onload = main;
 
+function main() {
+    addEventListeners()
+}
+
+function addEventListeners() {
+    const musicList = document.getElementById('music');
+    const mikaelaInfo = document.getElementById('mikaela')
+    
+    musicList.onclick = showSpotify;
+    mikaelaInfo.onclick = showMikaelaInfo;
+
+}
+
+function showSpotify() {
+    const spotifyList = document.getElementById('musicList');
+    const changeText = document.getElementById('changeText');
+    
+    changeText.innerHTML = spotifyList;
+}
+
+function showMikaelaInfo() {
+    const aboutMe = document.getElementById('mikaela');
+    const changeText = document.getElementById('changeText');
+    
+    changeText.innerHTML = aboutMe;
+}
