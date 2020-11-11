c562c21f75411d8ab24548e7f250d6a3ea2d5bf4
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-11 15:35:34 +0100

    changed back to normal

--- main.js
+++ main.js
@@ -46,18 +46,4 @@ function startSlideShowAnimation() {
 function showArtPictures() {
     const aboutArt = document.getElementById('aboutArt');
     changeText.innerHTML = aboutArt.innerHTML;
-
-    let liEls = document.querySelectorAll('ul, li');
-    let index = 0;
-    window.show = function(increase) {
-        index = index + increase;
-        index = Math.min(
-            Math.max(index,0),
-            liEls.length-1
-            );
-        liEls[index].scrollIntoView
-        ({behavior: 'smooth'});
-    }
-
-
 }
