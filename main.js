e71d01fcd643685246ba4f529ae167ab74bd6676
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-11 14:16:26 +0100

    trying to make slideshow work

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 window.addEventListener('load', main);
 
+
 function main() {
     addEventListeners();
 }
@@ -45,4 +46,18 @@ function startSlideShowAnimation() {
 function showArtPictures() {
     const aboutArt = document.getElementById('aboutArt');
     changeText.innerHTML = aboutArt.innerHTML;
+
+    let liEls = document.querySelectorAll('ul, li');
+    let index = 0;
+    window.show = function(increase) {
+        index = index + increase;
+        index = Math.min(
+            Math.max(index,0),
+            liEls.length-1
+            );
+        liEls[index].scrollIntoView
+        ({behavior: 'smooth'});
+    }
+
+
 }
