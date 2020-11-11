8838e2dcd801dfffd90014cdf3a19f128fa8e0be
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-11 12:06:46 +0100

    added JSDocs

--- main.js
+++ main.js
@@ -1,35 +1,13 @@
 
-
-// function main() {
-//     startSlideShowAnimation();
-// }
-
-// function startSlideShowAnimation() {
-//     setInterval(changeActiveImage, 2000);
-//         changeActiveImage();
-// }
-
-// function changeActiveImage() {
-//     const images = document.querySelectorAll('.slideshow-container > img');
-
-//         for(let i = 0; i < images.length; i++) {
-//             if (images[i].classList.contains('show')) {
-//                 images[i].classList.remove('show');
-
-//                 let nextIndex = (i + 1) % images.length;
-
-//                 images[nextIndex].classList.add('show');
-//                 break;
-//             }
-//         }
-
-
 window.addEventListener('load', main);
 
 function main() {
     addEventListeners();
 }
-
+/**
+ * 
+ * Getting pictures and the changingText id from HTML, making the pictures clickable.
+ */
 function addEventListeners() {
     let changeText = document.getElementById('changeText');
     const aboutMusicPicture = document.getElementById('music-picture');
@@ -44,43 +22,27 @@ function addEventListeners() {
     aboutArtPicture.addEventListener('click', showArtPictures);
     
 }
+
+/**
+ * Changing the changeText-id to the different content
+ */
+
 function showMusicInfo() {
     const aboutMusic = document.getElementById('aboutMusic');
     changeText.innerHTML = aboutMusic.innerHTML;
 }
-
 
 function showMikaelaInfo() { 
     const aboutMe = document.getElementById('aboutMe');
     changeText.innerHTML = aboutMe.innerHTML;  
 }
 
-
 function startSlideShowAnimation() {
     const aboutTravel = document.getElementById('aboutTravel');
-    changeText.innerHTML = aboutTravel.innerHTML;
-    setInterval(changeActiveImage, 2000);
-    changeActiveImage();
-    
+    changeText.innerHTML = aboutTravel.innerHTML;  
 }
 
 function showArtPictures() {
     const aboutArt = document.getElementById('aboutArt');
     changeText.innerHTML = aboutArt.innerHTML;
 }
-
-
-// function changeActiveImage() {
-//     const images = document.querySelectorAll('.travel-slideshow > img');
-        
-//         for(let i = 0; i < images.length; i++) {
-//             if (images[i].classList.contains('show')) {
-//                      images[i].classList.remove('show');
-        
-//                     let nextIndex = (i + 1) % images.length;
-        
-//                     images[nextIndex].classList.add('show');
-//                      break;
-//             }
-//         }
-// }
