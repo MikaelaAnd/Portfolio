d0d4305de23a4173cabcae592dcbbb7f63760d92
Mikaela Andersson <mikaelaaand@gmail.com>
2020-11-13 15:40:46 +0100

    made another slideshow

--- slideshow-art.js
+++ slideshow-art.js
@@ -1,17 +1,20 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+let slideIndex = [1, 1];
+let slideId = ["slide-pics1", "slide-pics2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
     let i;
-    let slides = document.getElementsByClassName('slide-pics');
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
+    let x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) { slideIndex[no] = 1 }
+    if (n < 1) { slideIndex[no] = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
+    x[slideIndex[no] - 1].style.display = "block";
 }
